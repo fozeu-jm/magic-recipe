e8a9b950157ebeeaa849421fe10b2eaa87410f91
FOZEU JEAN MARIE <fozeu.jm@gmail.com>
2020-08-04 16:46:10 +0200

    ok

--- src/app/recipes/RecipeService.service.ts
+++ src/app/recipes/RecipeService.service.ts
@@ -11,7 +11,7 @@ export class RecipeService{
 
     private recipes: Recipe[] = [
         new Recipe("Fried Chicken","Delicious chicken fries !", "https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13-1500x1125.jpg",
-        [ new Ingredient("Meat", 5), new Ingredient("French fries", 20) ]),
+        [ new Ingredient("Meat", 8), new Ingredient("French fries", 20) ]),
         new Recipe("Double Cheese Burger","The Double features two 100% pure", "http://www.pngall.com/wp-content/uploads/2016/05/Burger-Free-Download-PNG.png",[
             new Ingredient("Buns",2), new Ingredient("Meat", 1)
         ])
